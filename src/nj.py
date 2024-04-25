6431a58219f04d1f050a6e838dd31cf488b14462
grnimrod <nimrod.grandpierre@gmail.com>
2024-04-25 20:56:08 +0200

    Updated file

--- src/nj.py
+++ src/nj.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Bio import Phylo
 
 """
 We need to calculate the Q matrix at every iteration of the NJ algorithm
@@ -79,9 +80,9 @@ def calc_dist_from_orig_to_joined_node(index, dist_matrix):
     for k in range(dim):
         r_i += dist_matrix[i][k]
         r_j += dist_matrix[j][k]
-    
-    d_ki = (1/2) * (dist_matrix[i][j] + r_i - r_j)
-    d_kj = (1/2) * (dist_matrix[i][j] - r_i + r_j)
+
+    d_ki = round((1/2) * (dist_matrix[i][j] + r_i - r_j), 2)
+    d_kj = round((1/2) * (dist_matrix[i][j] - r_i + r_j), 2)
 
     return d_ki, d_kj
 
@@ -121,9 +122,74 @@ def update_dist_matrix(index, dist_matrix):
     return new_dist_matrix
 
 
-D, labels = initiate_dist_matrix("./data/example_slide4.phy")
-Q = calculate_Q(D)
-min_index = find_lowest_pair(Q)
-dist_to_joined = calc_dist_from_orig_to_joined_node(min_index, D)
-nd = update_dist_matrix(min_index, D)
-print(nd)
+def terminate(dist_matrix):
+    i, j, m = 0, 1, 2
+
+    d_vi = (1/2) * (dist_matrix[i][j] + dist_matrix[i][m] - dist_matrix[j][m])
+    d_vj = (1/2) * (dist_matrix[i][j] + dist_matrix[j][m] - dist_matrix[i][m])
+    d_vm = (1/2) * (dist_matrix[i][m] + dist_matrix[j][m] - dist_matrix[i][j])
+
+    return d_vi, d_vj, d_vm
+
+
+def newickify(node_to_children, root_node):
+    visited_nodes = set()
+
+    def newick_render_node(name, distance):
+        assert name not in visited_nodes, "Error: The tree may not be circular!"
+
+        if name not in node_to_children:
+            # Leafs
+            return F'{name}:{distance}'
+        else:
+            # Nodes
+            visited_nodes.add(name)
+            children = node_to_children[name]
+            children_strings = [newick_render_node(child, children[child]) for child in children.keys()]
+            children_strings = ",".join(children_strings)
+            return F'({children_strings}){name}:{distance}'
+
+    newick_string = newick_render_node(root_node, 0) + ';'
+
+    # Ensure no entries in the dictionary are left unused.
+    assert visited_nodes == set(node_to_children.keys()), "Error: some nodes aren't in the tree"
+
+    return newick_string
+
+
+def neighbor_joining(path_to_dist_matrix):
+    """
+    Create the input in Newick format somehow, then just make Phylo eat that and draw the tree
+    With each addition of a joined node, append an item to the 'labels' list
+    """
+    D, labels = initiate_dist_matrix(path_to_dist_matrix)
+    newick_dict = {}
+    counter = 1
+
+    while D.shape[0] > 1:
+        dict_nested = {}
+        Q = calculate_Q(D)
+        min_index = find_lowest_pair(Q)
+        print(f"Joining {min_index[0]} and {min_index[1]}")
+        print(f"List before joining nodes: {labels}")
+        dist_to_joined = calc_dist_from_orig_to_joined_node(min_index, D)
+        print(f"Distances: {dist_to_joined}")
+        dict_nested[labels[min_index[0]]] = dist_to_joined[0]
+        dict_nested[labels[min_index[1]]] = dist_to_joined[1]
+        inner_node = f"J{counter}"
+        newick_dict[inner_node] = dict_nested
+        labels.append(inner_node)
+        labels = [labels[i] for i in range(len(labels)) if i not in min_index]
+        new_dist_matrix = update_dist_matrix(min_index, D)
+        print("List after joining nodes")
+        print(labels)
+        D = new_dist_matrix
+        counter += 1
+        print(f"Tree in Newick format: {newick_dict}")
+    
+    newick_output = newickify(newick_dict, 'J4')
+
+    return newick_output
+
+
+neighbor_joining("./data/example_slide4.phy")
